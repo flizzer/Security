765b57a1490486e941ca6a7899e80c7098e3f44b
flizzer <bdavis@macpro.local>
2017-01-27 23:15:16 -0500

    adding pattern matching to not have to use name resolution where possible in order to speed up the output

--- GrizzlySteppeConnections/Get-GrizzlySteppeConnections.ps1
+++ GrizzlySteppeConnections/Get-GrizzlySteppeConnections.ps1
@@ -4,6 +4,15 @@ foreach ($remoteHost in $remoteHosts)
 {
     $remoteHostClean = $($remoteHost -replace "\[","" -replace "\]","" -replace ",","").Split("/")[0]
     write-host "Connections for $remoteHostClean..."
-    $netstatCommand =  netstat -a | Select-String $remoteHostClean 
+    $pattern = "^[0-9]"
+    $isNumeric = $remoteHost -match $pattern
+    if ($isNumeric)
+    {
+        $netstatCommand =  netstat -an | Select-String $remoteHostClean 
+    }
+    else 
+    {
+        $netstatCommand =  netstat -a | Select-String $remoteHostClean 
+    }
     Write-Host $netstatCommand
 }
